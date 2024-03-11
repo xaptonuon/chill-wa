0b09e89dd8e0e3aa20d0e5abb14ba6f6d44089bf
xaptonuon <99421101+xaptonuon@users.noreply.github.com>
2024-03-11 13:26:15 -0700

    even more last minute changes...

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://chill-wa.netlify.app',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
